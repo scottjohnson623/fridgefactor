4a214aef84e259c9d5aa1790cc75398794a3efe1
TwenLeMammouth <60606340+TwenLeMammouth@users.noreply.github.com>
2020-08-18 17:04:55 +0900

    Working on searchbar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 export default function Searchbar() {
+    const ingredientSearch = useRef()
+
     return (
-        <div>
-            <div>
-                <input type="text"></input>
-                
+        <div className="searchbar">
+            <div className="search">
+                <input ref={ingredientSearch} type="text"></input>
+
             </div>
+            <div>
             <button> StrictMode </button>
-            <button> Search ! </button>
+            <button className="buttonsearch"> Search ! </button>
+            </div>
         </div>
     )
 }
