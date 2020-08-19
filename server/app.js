cf561e34c70b9e90c6a2e48c42afdd9db58bda80
Ayumi Funaki <ayumifunaki@gmail.com>
2020-08-19 14:47:30 +0900

    finished user login and authentication

--- server/app.js
+++ server/app.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-
-const app = express();
 const API_KEY = process.env.API_KEY;
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const session = require("express-session");
+const app = express();
+const auth = require("./routes/auth");
+const user = require("./routes/user");
 
+app.use(express.static(path.join(__dirname, "..", "..", "client")));
+app.use(cookieParser(process.env.SECRET_KEY));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/auth", auth);
+app.use("/user", user);
+
+// recipes
 app.get("/recipes", async (req, res) => {
   const getAllRecipes = await axios({
     method: "GET",
@@ -29,7 +51,7 @@ app.get("/recipes/:ingredients", async (req, res) => {
     headers: {
       "content-type": "application/octet-stream",
       "x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
-      "x-rapidapi-key": API_KEY,
+      "x-rapidapi-key": process.env.API_KEY,
       useQueryString: true,
     },
     params: {
@@ -40,5 +62,20 @@ app.get("/recipes/:ingredients", async (req, res) => {
   });
   res.send(getFilteredRecipes.data);
 });
+
+// app.get("/user/receipes/", async (req, res) => {
+//   db.select().table('made_recipes');
+//   const getAllRecipes = await axios({
+//     method: "GET",
+//     url: "https://recipe-puppy.p.rapidapi.com/",
+//     headers: {
+//       "content-type": "application/json",
+//       "x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
+//       "x-rapidapi-key": API_KEY,
+//       useQueryString: true,
+//     },
+//   });
+//   res.send(getAllRecipes.data);
+// });
 
 module.exports = app;
