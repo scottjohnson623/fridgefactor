4424eabdc6d962d885133a2b2e4ac98b8e35377c
TwenLeMammouth <60606340+TwenLeMammouth@users.noreply.github.com>
2020-08-20 15:02:26 +0900

    Styling almost done..

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-
+import star from "../star.png"
 
 export default function RecipeCard(props) {
     return (
-        <div>
-            <h2>{props.recipe.title}</h2>
+        <div className="recipecard-wrap">
+            <h3>{props.recipe.title}</h3>
                 <div className="recipecontent">
                     <img className="recipeimage" src={props.recipe.thumbnail}></img>
-                    <h3>Ingredients: {props.recipe.ingredients}</h3>
+                    <p>Ingredients: {props.recipe.ingredients}</p>
                     
                 </div>
-                    <button>Share</button>
+                <div>
+                    <button class="buttoncard">Share</button>
+                    <button class="buttoncard"><img className="star" src={star}/></button>
+                    <button class="buttoncard">Made</button>
+                </div>
         </div>
     )
 }
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,17 +1,21 @@
 import React, { useRef } from 'react'
 
 export default function Searchbar() {
-    const ingredientSearch = useRef()
+    const ingredientSearch = useRef();
+    const foodSearch = useRef()
 
     return (
         <div className="searchbar">
             <div className="search">
-                <input ref={ingredientSearch} type="text"></input>
-
+                <input ref={ingredientSearch} type="text" placeholder="What do you have?"></input>
+                <button className="buttoncard"> + </button>
+                <input ref={foodSearch} type="text" placeholder="What do you want to make ?"></input>
+                <button className="buttoncard"> + </button>
             </div>
+
             <div className="buttons">
-            <button> StrictMode </button>
-            <button className="buttonsearch"> Search ! </button>
+                <button className="buttoncard"> StrictMode </button>
+                <button className="buttoncard"> Search ! </button>
             </div>
         </div>
     )
